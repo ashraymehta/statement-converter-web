84e1e6340cd159402a985a51ad615722196f3dca
ashray <ashraymehta@users.noreply.github.com>
2021-01-10 16:49:35 +0100

    Removed lottie.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { LottieModule } from 'ngx-lottie';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { LoaderComponent } from './loader/loader.component';
 import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
 import { StatementConverter } from '@ashray.mehta/statement-converter';
 import { DragDropDirective } from './directives/drag-drop/drag-drop.directive';
@@ -15,7 +13,6 @@ import { DragDropDirective } from './directives/drag-drop/drag-drop.directive';
     declarations: [
         AppComponent,
         HomeComponent,
-        LoaderComponent,
         FormComponent,
         DragDropDirective
     ],
@@ -23,8 +20,7 @@ import { DragDropDirective } from './directives/drag-drop/drag-drop.directive';
         BrowserModule,
         AppRoutingModule,
         NgbProgressbarModule,
-        ReactiveFormsModule,
-        LottieModule.forRoot({ player: () => import('lottie-web') })
+        ReactiveFormsModule
     ],
     providers: [FileReader, { provide: StatementConverter, useFactory: () => new StatementConverter() }],
     bootstrap: [AppComponent]
